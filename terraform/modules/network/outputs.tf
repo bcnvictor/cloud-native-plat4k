71c02f910a963fbb6ba40560aaaa536af56bf1b8
Victor <victor.biancini@gmail.com>
2026-05-06 16:39:37 +0200

    feat(10): refacto p1

--- terraform/modules/network/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "subnet_id" {
-  value = azurerm_subnet.subnet.id
-}
-output "nsg_id" {
-  value = azurerm_network_security_group.nsg.id
-}
--- terraform/modules/vm/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
-}
-output "fqdn" {
-  value = azurerm_public_ip.pip.fqdn
-}
-output "ssh_private_key_path" {
-  value = local_file.private_key.filename
-}
